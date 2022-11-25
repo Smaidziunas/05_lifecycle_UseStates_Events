509d8f4419c5f2eb4f1705c07fbce28df1c61376
Smaidziunas <sdamarcius@gmail.com>
2022-11-25 14:30:12 +0200

    Counter PushUps

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
 import './App.css';
 import Title from './components/Title';
 import MainTitle from './components/MainTitle';
+
+import Users from './components/Users';
+import { useState } from 'react';
 import Counter from './counters/Counter';
 
 function App() {
   console.log('App.jsx created');
+  const [showUsers, setShowUsers] = useState(true);
 
   // paspaudus an h1 el, padidini jo dydi iki 2rem
   return (
-    <div className='App'>
+    <div className='App container'>
+      <button onClick={() => setShowUsers(!showUsers)}>toggle Users</button>
       <MainTitle />
-      <Counter />
+
       {/* <Title>Events and Hooks</Title> */}
+      {/* <Counter /> */}
+      <Counter>Push Ups</Counter>
+
+      {/* {!showUsers && <Counter />}
+      {showUsers && <Users />} */}
     </div>
   );
 }
--- src/counters/Counter.jsx
+++ src/counters/Counter.jsx
@@ -1,7 +1,19 @@
 import { useState } from 'react';
 import './counter.css';
 
-function Counter() {
+function Counter(props) {
+  let cl = '';
+  let clm = '';
+
+  // function title() {
+  //   // return props.children;
+  //   if (props.children === '') {
+  //     return props.children === '';
+  //   } else {
+  //     return props.children;
+  //   }
+  // }
+
   // 1. State CounterValue, useState
   /*
   const stateArr = useState(0);
@@ -10,31 +22,55 @@ function Counter() {
   const counterValue = stateArr[0];
   const setCounterValue = stateArr[1];
   */
+
   const [counterValue, setCounterValue] = useState(0);
   // counterClass state;
-  const [counterClass, setCounterClass] = useState();
+  const [counterClass, setCounterClass] = useState('');
 
+  // tikrinam ar reiksme daugiau nei 5:
   // 2. increaseHandler, decreaseHandler fn kurios didins ir mazins skaitliuk
 
   function incrementHandler() {
     // grazinti previousCounterValue++ negerai, nes reikia grazinti pokyti, bet ne reiksme;
     setCounterValue((previousCounterValue) => {
       // patikrinti ar > 5 ir jeigu taip prideti klase high
-      return previousCounterValue + 1 > 5
-        ? setCounterClass('high')
-        : previousCounterValue + 1;
+      // return previousCounterValue + 1 > 5
+      //   ? setCounterClass('high')
+      //   : previousCounterValue + 1;
+      return previousCounterValue + 1 > -5 ? previousCounterValue + 1 : 0;
     });
+  }
+
+  function incrementHandlerMinus() {
+    setCounterValue((previousCounterValue) => {
+      return previousCounterValue - 1 > -5 ? previousCounterValue - 1 : 0;
+    });
+  }
+
+  if (counterValue > 5) {
+    // setCounterClass('high');
+    cl = 'high';
+    console.log(counterValue);
+  } else if (counterValue < 0) {
+    clm = 'low';
+  }
+
+  function resetFn() {
+    setCounterValue(0);
   }
 
   // !!! reiksme++ != reiksme + 1
 
   return (
     <div className='counter'>
-      <h3 className={`counter__value ${counterClass}`}>Counter</h3>
-      <h2>{counterValue}</h2>
+      <h3>{!props.children === true ? 'Counter' : props.children}</h3>
+      <h2 className={`counter__value ${cl} ${clm}`}>{counterValue}</h2>
       <div className='control'>
         <button onClick={incrementHandler}>+</button>
-        <button>-</button>
+        <button onClick={incrementHandlerMinus}>-</button>
+        <button className='resetBtn' onClick={resetFn}>
+          Reset
+        </button>
       </div>
     </div>
   );
